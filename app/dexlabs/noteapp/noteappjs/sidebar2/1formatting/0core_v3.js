4fbd36ef11ce6a54f8a38646f2bd1dabcdcbca45
vkdatta <vkyd2016@gmail.com>
2026-08-13 23:48:16 +0530

    Codebase Updated

--- app/dexlabs/noteapp/noteappjs/sidebar2/1formatting/0core_v3.js
+++ app/dexlabs/noteapp/noteappjs/sidebar2/1formatting/0core_v3.js
@@ -24,8 +24,8 @@ import {
   reverseWords,
   capitalizeWords,
   capitalizeSentences
-} from "./formatting_v2.js";
-import { registerFont, openFontPickerModal } from "./fontpicker_v2.js";
+} from "./formatting_v3.js";
+import { registerFont, openFontPickerModal } from "./fontpicker_v3.js";
 
 window.registerFont = registerFont;
 window.openFontPickerModal = openFontPickerModal;
