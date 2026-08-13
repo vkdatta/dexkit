2fe60d01a3493983314d0de2fa722d6965c5bdf5
vkdatta <vkyd2016@gmail.com>
2026-08-13 22:18:20 +0530

    Codebase Updated

--- app/dexlabs/noteapp/noteappjs/settings/0core_v2.js
+++ app/dexlabs/noteapp/noteappjs/settings/0core_v2.js
@@ -1,4 +1,4 @@
-import { createSettingsManager } from "./settings5.js";
+import { createSettingsManager } from "./settings_v2.js";
 
 const openSettings = createSettingsManager();
 if (openSettings) window.openSettingsManager = openSettings;
